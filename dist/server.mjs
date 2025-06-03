58eff6684a566cc05fda94f353ead06f80c8f2d8
naufal <naufalarhabi.mn@gmail.com>
2025-06-03 16:00:06 +0700

    add redirect config

--- dist/server.mjs
+++ dist/server.mjs
@@ -10,20 +10,15 @@ app.prepare().then(() => {
     const httpServer = createServer(handler);
     const io = new Server(httpServer);
     io.on("connection", (socket) => {
-        console.log("a user connected");
         socket.on("join-room", ({ room, username }) => {
             socket.join(room);
-            console.log(`${username} joined room: ${room}`);
             socket.to(room).emit("user-joined", `${username} has joined the room`);
         });
-        socket.on("message", ({ message, room, sender }) => {
-            console.log(`Message from ${sender} in room ${room}: ${message}`);
-            socket.to(room).emit("message", { sender, message });
+        socket.on("message", ({ message, room, sender, id }) => {
+            socket.to(room).emit("message", { sender, message, id });
         });
         // Disconnect event
-        socket.on("disconnect", () => {
-            console.log("user disconnected");
-        });
+        socket.on("disconnect", () => { });
     });
     httpServer.listen(port, () => {
         console.log(`> Ready on http://${hostname}:${port}`);
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: "/",        // path to match
+        destination: "/auth", // redirect target
+        permanent: true,   // or true if this should be a 308 permanent redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
